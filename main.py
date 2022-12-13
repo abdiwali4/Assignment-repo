32530cbba688409363972046e7e18760727220a1
abdiwali4 <abdiwalihassan13@gmail.com>
2022-12-13 09:27:59 -0800

    index.html

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-("print hello word")
-("whata fuck hell man")
-("sorry")
+<html>
+<head>
+<title> my assignment</title>
+</head>
+<body>
+<h4> canada</h4>
+<h5> usa </h5>
+<h6> luxinburg</h6>
+</body>
+</html>
